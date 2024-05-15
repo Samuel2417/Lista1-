46a468d6f1c86021649ab2476fc3c9ea6956811a
Samuel Lucas <168426129+Samuel2417@users.noreply.github.com>
2024-05-15 10:44:22 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,29 @@
+public class Animal2 {
+private String nome;
+private String tipo; 
+
+}
+
+public String getNome() {
+	return nome;
+	
+}
+
+public String getTipo() {
+	return tipo;
+	
+}
+
+public void setNome(String novoNome) {
+	this.nome = novoNome;
+	
+}
+
+public void setTipo(String novoTipo) {
+	this.tipo = novoTipo;
+	
+}
+
+}
+
+
