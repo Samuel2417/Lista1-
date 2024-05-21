26dfe136c27d185bdc1b15a52f4ad2669aa1bfba
Samuel Lucas <168426129+Samuel2417@users.noreply.github.com>
2024-05-21 10:23:55 -0300

    Create ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,30 @@
+public class Contacorrente {
+private int numeroDaConta;
+private double saldo;
+
+
+public int getNumeroDaConta() {
+	return numeroDaConta;
+	
+}
+
+public double getSaldo() {
+	return saldo;
+	
+}	
+
+public void setNumeroDaConta(String novoNumeroDaConta) {
+	this.numeroDaConta = novoNumeroDaConta;
+
+	
+}
+
+public void setSaldo(String novoSaldo) {
+	this.saldo = novoSaldo;
+	
+
+toString return saldo 
+
+}
+
+}
