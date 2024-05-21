d7e9fa55cb21acee352a947b7bb66d97d9a1eb16
Samuel Lucas <168426129+Samuel2417@users.noreply.github.com>
2024-05-21 10:19:54 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,40 @@
+public class Cachorro {
+private String nome;
+private String raca;
+private int idade;
+
+
+public String getNome() {
+	return nome;
+	
+}
+
+public String getRaca() {
+	return raca;
+	
+}
+
+public String getIdade() {
+	return idade;
+
+	
+}	
+
+public void setNome(String novoNome) {
+	this.nome = novoNome;
+	
+}
+
+public void setRaca(String novaRaca) {
+	this.raca = novaRaca;
+	
+}	
+	
+	
+public void setIdade(String novaIdade) {
+	this.idade = novaIdade;
+	
+	
+}
+	
+	
